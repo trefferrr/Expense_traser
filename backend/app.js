a71802e288a5d364c4239ad7e7c07407fb84b1ac
trefferrr <trefferx07@gmail.com>
2023-12-20 23:57:05 +0530

    backend commit 3

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+//middlewares
+const allowedOrigins = ["http://localhost:3000", "https://expense-gkt9ibroy-adityas-projects-6d993e50.vercel.app"];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+app.use(express.json());
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
