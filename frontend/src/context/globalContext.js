de54461f77398c3391d3e57dcca052528927549e
trefferrr <trefferx07@gmail.com>
2024-04-15 20:38:05 +0530

    Frontend modi

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from 'axios';
 
-const BASE_URL = "https://expense-tracer.onrender.com/api/v1/";
+const BASE_URL = "https://expense-traser.onrender.com/api/v1/";
 
 const GlobalContext = React.createContext();
 
