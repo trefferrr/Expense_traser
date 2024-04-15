d071b88c72442b845b856a037b0dbcb86eef1822
trefferrr <trefferx07@gmail.com>
2024-04-15 20:27:32 +0530

    Backend modi

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const db = async () => {
     try {
-        mongoose.set('strictQuery', true)
+        mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.DB)
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
     }
 }
-
 module.exports = {db}
